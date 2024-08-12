3494f6d2692661f8aa286f3992e421aed15ea2e8
Kaustubh-Dengle <dengle.kaustubh@gmail.com>
2024-08-12 23:53:51 +0530

    Line chart design completed

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -44,19 +44,29 @@ export default function LineChart() {
   return (
     <>
       <div className="w-2/3 p-4 rounded-xl shadow-md">
-        <div className="flex gap-4">
-          <button className="">1D</button>
-          <button>1W</button>
-          <button>1M</button>
-          <button>6M</button>
-          <button>1Y</button>
-          <div className="flex justify-items-end gap-4">
+        <div className="grid col-span-13 gap-2">
+          <button className="col-start-3 col-span-1 border-2 rounded-xl shadow-md hover:bg-blue-100 h-[54px]">
+            1D
+          </button>
+          <button className="col-start-4 col-span-1 border-2 rounded-xl shadow-md hover:bg-blue-100 h-[54px]">
+            1W
+          </button>
+          <button className="col-start-5 col-span-1 border-2 rounded-xl shadow-md hover:bg-blue-100 h-[54px]">
+            1M
+          </button>
+          <button className="col-start-6 col-span-1 border-2 rounded-xl shadow-md hover:bg-blue-100 h-[54px]">
+            6M
+          </button>
+          <button className="col-start-7 col-span-1 border-2 rounded-xl shadow-md hover:bg-blue-100 h-[54px]">
+            1Y
+          </button>
+          <div className="col-start-11 col-span-6 flex gap-4">
             <DropDown />
             <DropDown />
           </div>
         </div>
 
-        <div>
+        <div className="mt-4">
           <Line data={data} options={options} />
         </div>
       </div>
